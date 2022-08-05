0548711fef5d99905ffdddf47e3a55123f351045
Alexander Voinov <eji001@mail.ru>
2022-08-05 12:10:11 +0300

    Task001 (нулевая задача) - квадрат числа

---
+++ Task001/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат 
+//(число умноженное на само себя).
+
+//Например:
+//4 -> 16 
+//-3 -> 9 
+//-7 -> 49
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32( Console.ReadLine());
+int result = number*number ;
+
+Console.WriteLine("квадрат числа равен: ");
+Console.WriteLine(result);
