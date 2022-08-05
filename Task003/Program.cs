0f4e6210d8d591f13f284f72be5c0e6d396bedc2
Alexander Voinov <eji001@mail.ru>
2022-08-05 13:33:32 +0300

    Task003 (третья задача) день недели по порядковому номеру

---
+++ Task003/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая будет выдавать название дня недели 
+//по заданному номеру.
+//	3 -> Среда 
+//  5 -> Пятница
+
+Console.WriteLine("введите номер дня недели");
+int number = Convert.ToInt32( Console.ReadLine());
+
+if(number<1 || number>7){
+    Console.WriteLine("введите чисдл от 1 до 7");    
+} else if( number == 1){ Console.WriteLine("понедельник");    
+} else if( number == 2){ Console.WriteLine("вторник");    
+} else if( number == 3){ Console.WriteLine("среда");    
+} else if( number == 4){ Console.WriteLine("четверг");    
+} else if( number == 5){ Console.WriteLine("пятница");    
+} else if( number == 6){ Console.WriteLine("суббота");    
+} else if( number == 7){ Console.WriteLine("воскреаение");    
+}
