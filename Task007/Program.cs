32632a59487086994af531d0c56dc761456d445e
Alexander Voinov <eji001@mail.ru>
2022-08-05 17:46:42 +0300

    Task007 - дз (четвертая задача) максимальное из 3 чисел

---
+++ Task007/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа 
+//  и выдаёт максимальное из этих чисел.
+
+//  2, 3, 7 -> 7
+//  44 5 78 -> 78
+//  22 3 9 -> 22
+
+
+Console.WriteLine("введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите 3 число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max=a;
+
+if(max<b){max=b;}
+if(max<c){max=c;}
+
+Console.WriteLine("max = "+max);
