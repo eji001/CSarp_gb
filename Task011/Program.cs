c74f3b2e3e92a0bc2aa1c13b7e7bf7c1cbb20327
Alexander Voinov <eji001@mail.ru>
2022-08-08 22:25:45 +0300

    Task011 (одинадцатая задача) удалить среднюю цифру случайного трехзначного числа

---
+++ Task011/Program.cs
@@ -0,0 +1,15 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int number = new Random().Next(100, 1000); // [10, 99]
+Console.WriteLine(number);
+
+int res1 = number / 100;
+int res3 = number % 10;
+int finres = res1 * 10 + res3;
+
+Console.WriteLine(finres);
