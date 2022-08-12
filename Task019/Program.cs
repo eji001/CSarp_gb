620e46ce070587d3f62e63e5ae026c13fb058fba
Alexander Voinov <eji001@mail.ru>
2022-08-12 13:51:35 +0300

    Task019 (восемнадцатая задача) вывод возможных координат по номеру четверти

---
+++ Task019/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+//
+
+
+
+void Qarter(int n)
+{
+    if (n < 1 || n > 4) Console.WriteLine("не коректен номер четверти");
+    else if (n == 1) Console.WriteLine(n + " => x > 0 && y > 0");
+    else if (n == 2) Console.WriteLine(n + " => x < 0 && y > 0");
+    else if (n == 3) Console.WriteLine(n + " => x < 0 && y < 0");
+    else if (n == 4) Console.WriteLine(n + " => x > 0 && y < 0");
+}
+
+Qarter(5);
+Qarter(4);
+Qarter(3);
+Qarter(2);
+Qarter(1);
+Qarter(0);
