42e0114d5c5d73c8b11da7d7eb9e828902e6a0f1
Alexander Voinov <eji001@mail.ru>
2022-08-16 21:27:20 +0300

    Task027 (двадцать восьмая задача) произведение чисел от 1 до N

---
+++ Task027/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+
+//4 -> 24 
+//5 -> 120
+
+int Factorial(int n)
+{
+    int result=1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;        
+    }
+    return result;
+}
+
+void PrintFactorial(int n)
+{
+    Console.WriteLine($"произведение чисел от 1 до {n} равно {Factorial(n)}");
+
+}
+
+PrintFactorial(4);
+PrintFactorial(5);
