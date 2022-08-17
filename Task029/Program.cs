37498add335ca3cb72124e6a6e3953d88194fe8b
Alexander Voinov <eji001@mail.ru>
2022-08-17 14:12:32 +0300

    Task029 - дз (25 задача) A в степени B ввод зациклен

---
+++ Task029/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void Exponentiation()
+{
+
+    string ExitFlag = string.Empty;
+    bool flag = true;
+    int res = 1;
+    while (flag)
+    {
+        Console.Write("введите A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.Write("введите B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        if (B > 0) // натуральеая степень 1,2,3 ... 
+        {
+            for (int i = 0; i < B; i++)
+            {
+                res *= A;
+                //Console.WriteLine(res + "-" + A);
+            }
+            Console.WriteLine($"{A} в степени {B} == {res}");
+            res = 1;
+        }
+        else if (B <= 0)
+        {
+            Console.WriteLine($"введите В > 0");
+        }
+
+
+        //зацикленность 
+        Console.WriteLine("продолжить цикл (yes), введите y");
+        ExitFlag = Convert.ToString(Console.ReadLine());
+        if (ExitFlag != "y")
+        {
+            flag = false;
+        }
+    }
+}
+
+Exponentiation();
---
+++ Task030/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int SumOfNumber(int n)
+{
+    int summ = 0;
+    while (n > 0)
+    {
+        summ += n % 10;
+        n /= 10;
+    }
+
+    return summ;
+}
+
+void PrintSumOfNumber(int num)
+{
+    Console.WriteLine($"суммф чисел в чмсле {num} равна {SumOfNumber(num)}");
+}
+
+PrintSumOfNumber(452);
+PrintSumOfNumber(82);
+PrintSumOfNumber(9012);
