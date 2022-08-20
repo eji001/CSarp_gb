5e5143c04fb20e641c7e6cc14b585819321a0ccd
Alexander Voinov <eji001@mail.ru>
2022-08-20 18:11:09 +0300

    Task035 (35 задача) найти количество элементов массива в отрезке [10,99]

---
+++ Task035/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+
+///Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+// инициализация массива
+int[] CreateArray(int length, int min, int max)
+{
+    int[] myarr = new int[length];
+    for (int i = 0; i < myarr.Length; i++)
+    {
+        myarr[i] = new Random().Next(min, max + 1);    //(-9,10)       (min, max+1) 
+    }
+    return myarr;
+}
+
+// печать массива
+void PrintArray(int[] myarr)
+{
+    Console.Write($"массив из {myarr.Length} символов [");
+    for (int i = 0; i < myarr.Length; i++)
+    {
+        Console.Write($"{myarr[i]}");
+        if (i < myarr.Length - 1)
+            Console.Write($",");
+    }
+    Console.WriteLine("]");
+}
+
+int numderInInterval(int[] arr)
+{
+    int num = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] < 100)
+            num++;
+    }
+    return num;
+
+}
+
+
+//вывод
+int[] arr = CreateArray(123, 0, 499);
+PrintArray(arr);
+
+Console.WriteLine($"в интервале (10-99) нашего масива найдено {numderInInterval(arr)} чисел");//
