494ef591ce68ff8a136dbf8f5e19112e4103c3d5
Alexander Voinov <eji001@mail.ru>
2022-08-20 18:04:57 +0300

    Task032 (31 задача) сумма отрицательных и положительных элементов массива

---
+++ Task032/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+
+//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+// сумма положительных чисел равна 29, 
+// сумма отрицательных равна -20.
+
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] myarr = new int[length];
+    for (int i = 0; i < myarr.Length; i++)
+    {
+        myarr[i] = new Random().Next(min, max + 1);    //(-9,10)       
+    }
+    return myarr;
+}
+
+void PrintArray(int[] myarr)
+{
+    Console.Write($"массив из {myarr.Length} символов [");
+    for (int i = 0; i < myarr.Length; i++)
+    {
+        Console.Write($"{myarr[i]}");
+        if (i < myarr.Length - 1)
+            Console.Write($",");
+    }
+    Console.WriteLine("]");
+}
+
+int FindPositiveSun(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0)
+            sum += arr[i];
+    }
+    return sum;
+}
+
+
+int FindNegativeSun(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            sum += arr[i];
+    }
+    return sum;
+}
+
+int[] arr = CreateArray(12, -9, 9);
+PrintArray(arr);
+
+Console.WriteLine($"сумма отрицательных элементов массива {FindPositiveSun(arr)}");
+Console.WriteLine($"сумма положительных элементов массива {FindNegativeSun(arr)}");
