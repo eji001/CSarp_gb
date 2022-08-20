249fc499366dea2a547afd1074a18cf13acab3d0
Alexander Voinov <eji001@mail.ru>
2022-08-20 18:02:37 +0300

    блоки кода

--- для копипасты.cs
+++ для копипасты.cs
@@ -1,10 +1,22 @@
-//28 
+//32 
 
+
+
+// инициализация массива
+// min- включается , max - не включается
+int[] CreateArray(int length, int min , int max)
+{
+    int[] myarr = new int[length];
+    for (int i = 0; i < myarr.Length; i++)
+    {
+        myarr[i] = new Random().Next(min, max+1);    // (min, max+1) 
+    }
+    return myarr;
+}
 
 // печать массива
-void PrintArr(int num)
-{
-    int[] myarr = arr(num);
+void PrintArray(int[] myarr)
+{    
     Console.Write($"массив из {myarr.Length} символов [");
     for (int i = 0; i < myarr.Length; i++)
     {
@@ -14,3 +26,10 @@ void PrintArr(int num)
     }
     Console.WriteLine("]");
 }
+
+//вывод
+int[] arr = CreateArray(12,-9,9);
+PrintArray(arr);
+
+//Console.WriteLine($" {}");
+//Console.WriteLine($" {(arr)}");
