c434c343877b1a0f2a3e1ac51eadb09f11d3736f
Alexander Voinov <eji001@mail.ru>
2022-08-27 00:47:11 +0300

    блоки кода

--- для копипасты.cs
+++ для копипасты.cs
@@ -4,19 +4,19 @@
 
 // инициализация массива
 // min- включается , max - не включается
-int[] CreateArray(int length, int min , int max)
+int[] CreateArray(int length, int min, int max)
 {
     int[] myarr = new int[length];
     for (int i = 0; i < myarr.Length; i++)
     {
-        myarr[i] = new Random().Next(min, max+1);    // (min, max+1) 
+        myarr[i] = new Random().Next(min, max + 1);    // (min, max+1) 
     }
     return myarr;
 }
 
 // печать массива
 void PrintArray(int[] myarr)
-{    
+{
     Console.Write($"массив из {myarr.Length} символов [");
     for (int i = 0; i < myarr.Length; i++)
     {
@@ -28,8 +28,41 @@ void PrintArray(int[] myarr)
 }
 
 //вывод
-int[] arr = CreateArray(12,-9,9);
+int[] arr = CreateArray(12, -9, 9);
 PrintArray(arr);
 
 //Console.WriteLine($" {}");
 //Console.WriteLine($" {(arr)}");
+
+//---------------------------------------------
+//двумерный массив 
+// rowCount количество строк / columnCount количество столбцов
+
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int colunm = 0; colunm < columnCount; colunm++)
+        {
+            array[row, colunm] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+//
+void PrintMatrix(int[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int colunm = 0; colunm < arr.GetLength(1); colunm++)
+        {
+            Console.Write($"{arr[row, colunm]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(5, 3, 0, 99);
+PrintMatrix(matrix);
