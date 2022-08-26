ddda4fa21eb3e87d2f59ad7a1040a34cef01739f
Alexander Voinov <eji001@mail.ru>
2022-08-27 01:21:44 +0300

    Task050 (51 задача) найти сумму элементов по диагонали

---
+++ Task050/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+
+void DiagonalSummMatrix(int[,] arr)
+{
+    int sum = 0;
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int colunm = 0; colunm < arr.GetLength(1); colunm++)
+        {
+            if (row == colunm)
+            {
+                sum += arr[row, colunm];
+            }
+        }
+    }
+
+    Console.WriteLine($"Сумма элементов главной диагонали: = {sum}");
+
+}
+//------------
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int colunm = 0; colunm < columnCount; colunm++)
+        {
+            array[row, colunm] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+//
+void PrintMatrix(int[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int colunm = 0; colunm < arr.GetLength(1); colunm++)
+        {
+            Console.Write($"{arr[row, colunm]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 4, 5, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+DiagonalSummMatrix(matrix);
