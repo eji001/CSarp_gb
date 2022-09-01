bfdc9d42b4fc66b1af882a25da3aec5d5ee6382d
Alexander Voinov <eji001@mail.ru>
2022-09-01 06:41:32 +0300

    Task063 (62 задача) - дз Заполните спирально массив 4 на 4. (...)

---
+++ Task063/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+
+// 1  2  3  4
+// 12 13 14 5
+// 11 16 15 6
+// 10  9  8 7
+
+//??
+
+
+int[,] SpiralMatrix(int n)
+{
+    int[,] array = new int[n, n];
+
+    int num = 1;
+    int value = -n;//
+    int sum = -1;//
+
+    for (int count = n; count > 0; )
+    {
+        value = -1 * value / n;//
+        for (int i = 0; i < count; i++)
+        {
+            sum += value;
+            array[sum / n, sum % n] = num++;//
+        }
+        value *= n;//
+        count--;
+        for (int i = 0; i < count; i++)
+        {
+            sum += value;//
+            array[sum / n, sum % n] = num++;//
+        }
+    }
+
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int colunm = 0; colunm < arr.GetLength(1); colunm++)
+        {
+            Console.Write($"{arr[row, colunm]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int[,] matrix = SpiralMatrix(4);
+PrintMatrix(matrix);
+Console.WriteLine();
