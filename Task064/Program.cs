75caf4cf1b7dc3db66f2550a5a83baee6f942bfe
Alexander Voinov <eji001@mail.ru>
2022-09-03 06:42:48 +0300

    Task064 (63 задача) рекурсивно вывести все натуральные числа в промежутке от 1 до N

---
+++ Task064/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63: Задайте значение N. Напишите программу,
+//которая выведет все натуральные числа в промежутке от 1 до N.
+
+//N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void PrintNumbers(int start, int end)
+{
+    if (start > end)
+        return;
+
+    Console.Write($"{start} ");
+    PrintNumbers(start + 1, end);
+}
+
+// int number=Convert.ToInt32(Console.ReadLine());
+
+PrintNumbers(1, 5);
