52c12e6f48fe216f494c06988cbb20291a371841
Alexander Voinov <eji001@mail.ru>
2022-09-03 06:47:44 +0300

    Task067 (69 задача) рекурсивно вывести возведение числа в степень

---
+++ Task067/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B,
+//и возводит число А в целую степень B с помощью рекурсии.
+
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// -1.27
+
+int Exponentiation(int A, int B)
+{
+    if (B == 0)
+        return 1;
+    
+    return A * Exponentiation(A, B - 1);
+}
+
+Console.WriteLine(Exponentiation(3, 5));
