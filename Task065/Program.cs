0ba8eba63706acb7ddf15287c338502629223029
Alexander Voinov <eji001@mail.ru>
2022-09-03 06:43:56 +0300

    Task065 (65 задача) рекурсивно вывести все натуральные числа в промежутке от M до N

---
+++ Task065/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 65: Задайте значения M и N.
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+//M = 1; N = 5 -> "1, 2, 3, 4, 5"
+//M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+void PrintNumbers(int start, int end)
+{
+    if (start > end)
+        return;
+
+    Console.Write($"{start} ");
+    PrintNumbers(start + 1, end);
+}
+
+PrintNumbers(3, 8);
